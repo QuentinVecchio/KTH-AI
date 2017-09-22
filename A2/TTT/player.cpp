8b289ccd6740147b8889781eb7759acdc0ec361e
Quentin Vecchio <quentin.vecchio@insa-lyon.fr>
2017-09-22 16:43:35 +0200

    Ajout solution A2 TTT

--- A2/TTT/player.cpp
+++ A2/TTT/player.cpp
@@ -1,13 +1,29 @@
 #include "player.hpp"
 #include <cstdlib>
-#include <climits>
+#include <limits>
+#include <iostream>
+#include <stdint.h>
+
+#define LARGEUR 4
+#define HAUTEUR 4
+
+#define DEPTH 4
+
+#define MINIMUM std::numeric_limits<int>::min()
+#define MAXIMUM std::numeric_limits<int>::max()
 
 namespace TICTACTOE
 {
 
+Player::Player() {
+    this->states[CELL_X] = std::map<std::string, int>();
+    this->states[CELL_O] = std::map<std::string, int>();
+}
+
 GameState Player::play(const GameState &pState,const Deadline &pDue)
 {
-    this->states.clear();
+    this->states[CELL_X].clear();
+    this->states[CELL_O].clear();
     this->count = 0;
     std::vector<GameState> lNextStates;
     pState.findPossibleMoves(lNextStates);
@@ -15,12 +31,12 @@ GameState Player::play(const GameState &pState,const Deadline &pDue)
     if (lNextStates.size() == 0) return GameState(pState, Move());
 
     unsigned indexBestMove = 0;
-    unsigned bestEvaluation = 0;
+    int bestEvaluation = MINIMUM;
 
     std::cerr << "Analyze possibilities" << std::endl;
     for(unsigned i=0;i<lNextStates.size();++i) {
-        //int v = this->minMax(lNextStates[i], 1, 4);
-        int v = this->alphaBeta(lNextStates[i], 5, INT_MIN, INT_MAX, 1);
+        //int v = this->minMax(lNextStates[i], pState.getNextPlayer(), 3);
+        int v = this->alphaBeta(lNextStates[i], pState.getNextPlayer(), 15, MINIMUM, MAXIMUM);
         if(v>bestEvaluation) {
             bestEvaluation = v;
             indexBestMove = i;
@@ -32,10 +48,12 @@ GameState Player::play(const GameState &pState,const Deadline &pDue)
     return lNextStates[indexBestMove];
 }
 
-int Player::gain(int nbCell, int player) const {
+int Player::gain(int nbCell, bool currentPlayer) const {
     int coef = 1;
-    if(player == 1)
+    if(currentPlayer && nbCell == 4)
         coef = -1;
+    else if(currentPlayer)
+        coef = 0;
     switch(nbCell) {
         case 1:
             return coef;
@@ -50,10 +68,12 @@ int Player::gain(int nbCell, int player) const {
     }
 }
 
-int Player::eval(GameState state, int player) const {
+int Player::eval(GameState state, uint8_t player) const {
     int sum = 0;
-    Cell valueMine = CELL_X;
+    Cell valueMine = Cell(player);
     Cell valueOpp = CELL_O;
+    if(valueMine == CELL_O)
+        valueOpp = CELL_X;
     int nbMine;
     int nbOpp;
     //rows
@@ -67,8 +87,8 @@ int Player::eval(GameState state, int player) const {
                 nbOpp++;
             }
         }
-        sum += this->gain(nbMine, 0);
-        sum += this->gain(nbOpp, 1);
+        sum += this->gain(nbMine, player == state.getNextPlayer());
+        sum += this->gain(nbOpp, player == state.getNextPlayer());
     }
     //columns
     for(unsigned i=0;i<4;i++) {
@@ -81,10 +101,10 @@ int Player::eval(GameState state, int player) const {
                 nbOpp++;
             }
         }
-        sum += this->gain(nbMine, 0);
-        sum += this->gain(nbOpp, 1);
+        sum += this->gain(nbMine, player == state.getNextPlayer());
+        sum += this->gain(nbOpp, player == state.getNextPlayer());
     }
-    //diagonals left-top to rigth-bottom
+    ///diagonals left-top to rigth-bottom
     for(unsigned i=0;i<4;i++) {
         nbMine = 0;
         nbOpp = 0;
@@ -93,61 +113,60 @@ int Player::eval(GameState state, int player) const {
         } else if(state.at(i, i) == valueOpp) {
             nbOpp++;
         }
-        sum += this->gain(nbMine, 0);
-        sum += this->gain(nbOpp, 1);
+        sum += this->gain(nbMine, player == state.getNextPlayer());
+        sum += this->gain(nbOpp, player == state.getNextPlayer());
     }
     //diagonals rigth-top to left-bottom
     for(unsigned i=0;i<4;i++) {
         nbMine = 0;
         nbOpp = 0;
-        if(state.at(i, 4-i) == valueMine) {
+        if(state.at(i, 3-i) == valueMine) {
             nbMine++;
-        } else if(state.at(i, 4-i) == valueOpp) {
+        } else if(state.at(i, 3-i) == valueOpp) {
             nbOpp++;
         }
-        sum += this->gain(nbMine, 0);
-        sum += this->gain(nbOpp, 1);
+        sum += this->gain(nbMine, player == state.getNextPlayer());
+        sum += this->gain(nbOpp, player == state.getNextPlayer());
     }
     return sum;
 }
 
-int Player::minMax(GameState state, int player, int depth) {
+int Player::minMax(GameState state, uint8_t player, int depth) {
+    uint8_t nextPlayer = state.getNextPlayer();
+    std::vector<GameState> possibleMoves;
+    state.findPossibleMoves(possibleMoves);
     if(state.isEOG() || depth == 0) {
         return this->eval(state, player);
     } else {
         this->count++;
         int bestPossible;
-        if(player == 0) {
-            bestPossible = INT_MIN;
-            std::vector<GameState> possibleMoves;
-            state.findPossibleMoves(possibleMoves);
+        if(player != nextPlayer) {
+            bestPossible = MINIMUM;
             for(unsigned i=0;i<possibleMoves.size();++i) {
-                int v;
-                if(!this->stateExist(possibleMoves[i])) {
-                    v = minMax(possibleMoves[i], 0, depth-1);
-                    this->createState(state, v);
+                int tmp;
+                if(!this->stateExist(player, possibleMoves[i])) {
+                    tmp = minMax(possibleMoves[i], player, depth-1);
+                    this->createState(player, possibleMoves[i], tmp);
                 } else {
-                    v = this->getGainForState(possibleMoves[i]);
+                    tmp = this->getGainForState(player, possibleMoves[i]);
                 }
-                if(v>bestPossible) {
-                    bestPossible = v;
+                if(tmp>bestPossible) {
+                    bestPossible = tmp;
                 }
             }
             return bestPossible;
         } else {
-            bestPossible = INT_MAX;
-            std::vector<GameState> possibleMoves;
-            state.findPossibleMoves(possibleMoves);
+            bestPossible = MAXIMUM;
             for(unsigned i=0;i<possibleMoves.size();++i) {
-                int v;
-                if(!this->stateExist(possibleMoves[i])) {
-                    v = minMax(possibleMoves[i], 0, depth-1);
-                    this->createState(state, v);
+                int tmp;
+                if(!this->stateExist(player, possibleMoves[i])) {
+                    tmp = minMax(possibleMoves[i], player, depth-1);
+                    this->createState(player, possibleMoves[i], tmp);
                 } else {
-                    v = this->getGainForState(possibleMoves[i]);
+                    tmp = this->getGainForState(player, possibleMoves[i]);
                 }
-                if(v<bestPossible) {
-                    bestPossible = v;
+                if(tmp<bestPossible) {
+                    bestPossible = tmp;
                 }
             }
         }
@@ -155,65 +174,64 @@ int Player::minMax(GameState state, int player, int depth) {
     }
 }
 
-int Player::alphaBeta(GameState state, int depth, int alpha, int beta, int player) {
-    //return 0;
-    int v;
+int Player::alphaBeta(GameState state, uint8_t player, unsigned depth, int alpha, int beta) {
+    uint8_t nextPlayer = state.getNextPlayer();
+    int nextDepth = depth;
+    nextDepth--;
+    /*if(factoriel(16-dept) > 3000) {
+        
+    }*/
+    std::vector<GameState> possibleMoves;
+    state.findPossibleMoves(possibleMoves);
+    this->count++;
     if(depth == 0 || state.isEOG()) {
         return this->eval(state, player);
-    } else if(player == 0) {
-        v = INT_MIN;
-        this->count++;
-        std::vector<GameState> possibleMoves;
-        state.findPossibleMoves(possibleMoves);
+    } else if(player == nextPlayer) {
+        int max = MINIMUM;
         for(unsigned i=0;i<possibleMoves.size();++i) {
-            if(!this->stateExist(possibleMoves[i])) {
-                int val;
-                if(!this->stateExist(possibleMoves[i])) {
-                    val = alphaBeta(possibleMoves[i], depth-1, alpha, beta, 1);
-                    this->createState(state, val);
-                } else {
-                    val = this->getGainForState(possibleMoves[i]);
-                }
-                if(val>v) {
-                    v = val;
-                }
-                if(v>alpha) {
-                    alpha = v;
-                }
-                if(beta<=alpha) {
-                    return v;
-                }
+            int tmp;
+            if(!this->stateExist(player, possibleMoves[i])) {
+                tmp = alphaBeta(possibleMoves[i], player, nextDepth, alpha, beta);
+                this->createState(player, possibleMoves[i], tmp);
+            } else {
+                tmp = this->getGainForState(player, possibleMoves[i]);
+            }
+            if(tmp > max) {
+                max = tmp;
+            }
+            if(max > alpha) {
+                alpha = max;
+            }
+            if(beta<=alpha) {
+                break;
             }
         }
-    } else if(player == 1) {
-        v = INT_MAX;
-        std::vector<GameState> possibleMoves;
-        state.findPossibleMoves(possibleMoves);
+        return max;
+    } else {
+        int min = MAXIMUM;
         for(unsigned i=0;i<possibleMoves.size();++i) {
-            if(!this->stateExist(possibleMoves[i])) {
-                int val;
-                if(!this->stateExist(possibleMoves[i])) {
-                    val = alphaBeta(possibleMoves[i], depth-1, alpha, beta, 0);
-                    this->createState(state, val);
-                } else {
-                    val = this->getGainForState(possibleMoves[i]);
-                }
-                if(val<v) {
-                    v = val;
-                }
-                if(v<beta) {
-                    beta = v;
-                }
-                if(beta<=alpha) {
-                    return v;
-                }
+            int tmp;
+            if(!this->stateExist(player, possibleMoves[i])) {
+                tmp = alphaBeta(possibleMoves[i], player, nextDepth, alpha, beta);
+                this->createState(player, possibleMoves[i], tmp);
+            } else {
+                tmp = this->getGainForState(player, possibleMoves[i]);
+            }
+            if(tmp<min) {
+                min = tmp;
+            }
+            if(min<beta) {
+                beta = min;
+            }
+            if(beta<=alpha) {
+                break;
             }
         }
+        return min;
     }
-    return v;
 }
 
-bool Player::stateExist(GameState state) const {
+bool Player::stateExist(unsigned player, GameState state) const {
     // Test the first matrice    
     int **matrice = new int*[4];
     for(unsigned x=0;x<4;x++) {
@@ -228,7 +246,7 @@ bool Player::stateExist(GameState state) const {
             ss << MESSAGE_SYMBOLS[matrice[x][y]];
         }
     }
-    if(this->states.find(ss.str()) != this->states.end()) {
+    if(this->states.find(player)->second.find(ss.str()) != this->states.find(player)->second.end()) {
         //std::cerr << "Matrice exists " << std::endl;
         return true;
     }
@@ -241,7 +259,7 @@ bool Player::stateExist(GameState state) const {
                 ss << MESSAGE_SYMBOLS[matriceRotated[x][y]];
             }
         }
-        if(this->states.find(ss.str()) != this->states.end()) {
+        if(this->states.find(player)->second.find(ss.str()) != this->states.find(player)->second.end()) {
             //std::cerr << "Matrice rotated exists " << std::endl;
             return true;
         } else {
@@ -251,19 +269,19 @@ bool Player::stateExist(GameState state) const {
     return false;
 }
 
-int Player::getGainForState(GameState state) const {
+int Player::getGainForState(unsigned player, GameState state) const {
     std::stringstream ss;
     for(int i=0;i<state.cSquares;i++)
         ss << MESSAGE_SYMBOLS[state.at(i)];
-    return this->states.find(ss.str())->second;
+    return this->states.find(player)->second.find(ss.str())->second;
 }
 
-void Player::createState(GameState state, int value) {
+void Player::createState(unsigned player, GameState state, int value) {
     std::stringstream ss;
     for(int i=0;i<state.cSquares;i++)
         ss << MESSAGE_SYMBOLS[state.at(i)];
     //std::cerr << "Analyse " << ss.str() << std::endl;
-    this->states.insert(std::pair<std::string, int>(ss.str(), value));
+    this->states.find(player)->second.insert(std::pair<std::string, int>(ss.str(), value));
 }
 
 int** Player::matriceRotation(int** matrice, int n, int m) const {
@@ -279,6 +297,12 @@ int** Player::matriceRotation(int** matrice, int n, int m) const {
     return newMatrice;
 }
 
-
+int Player::factoriel(int n) {
+    int f = 1;
+    for(int i=n;i>0;i--) {
+        f *= i;
+    }
+    return f;
+}
 /*namespace TICTACTOE*/ 
 }
--- A2/TTT/player.hpp
+++ A2/TTT/player.hpp
@@ -14,9 +14,11 @@ namespace TICTACTOE
 class Player
 {
 private:
-    std::map<std::string, int> states;
+    std::map<int, std::map<std::string, int> > states;
     int count;
+
 public:
+    Player();
     ///perform a move
     ///\param pState the current state of the board
     ///\param pDue time before which we must have returned
@@ -24,14 +26,16 @@ public:
     GameState play(const GameState &pState, const Deadline &pDue);
 
 private:
-    int minMax(GameState state, int player, int depth);
-    int alphaBeta(GameState state, int depth, int alpha, int beta, int player);
-    int eval(GameState state, int player)  const;
-    bool stateExist(GameState state) const;
-    void createState(GameState state, int v);
+    int minMax(GameState state, uint8_t player, int depth);
+    //int alphaBeta(GameState state, int depth, int alpha, int beta, uint8_t player);
+    int alphaBeta(GameState state, uint8_t player, unsigned depth, int alpha, int beta);
+    int eval(GameState state, uint8_t player)  const;
+    bool stateExist(unsigned player, GameState state) const;
+    void createState(unsigned player, GameState state, int v);
     int** matriceRotation(int** matrice, int n, int m) const;
-    int getGainForState(GameState state) const;
-    int gain(int nbCell, int player) const;
+    int getGainForState(unsigned player, GameState state) const;
+    int gain(int nbCell, bool currentPlayer) const;
+    int factoriel(int n);
 };
 
 /*namespace TICTACTOE*/ }
